ce51cd07872f243247360497e251439621e279fe
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-25 09:17:29 +0200

    add changes

--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('booking.urls')),
+    # url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    # url(r'^ticket/(?P<ticket_id>(\d+))$', generate_view ),
 ]
+
