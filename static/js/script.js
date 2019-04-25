1fc5749e97b95759c4a59c70d0c70399f391df93
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-25 09:19:16 +0200

    add changes

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".toggle-details").click(function(){
-      $(this).text(function(i, text){
-        return text === "Show Details" ? "Hide Details" : "Show Details";
-      })
-      $(".collapse").collapse('toggle');
-  });
-  
-});
